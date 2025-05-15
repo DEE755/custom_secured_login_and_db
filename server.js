612c1689c9c8a04b01ba13b369d39c0414c8db2b
dorian <dualdproductions@gmail.com>
2025-05-15 18:56:18 +0300

    prepare for render2 simula

--- server.js
+++ server.js
@@ -15,13 +15,6 @@ app.use(bodyParser.json());
 
 
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database.');
-});
 
 // Route to handle form submission
 app.post('/api/submit', (req, res) => {
